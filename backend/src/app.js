d7ac386bd9f013a64e2c60911c52a6f634752bf2
Lucasilva88 <Lucasilva999@hotmail.com.br>
2020-03-29 19:22:37 -0300

    Adicionado o Front-End da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,15 +6,13 @@ const porta = process.env.PORTA || 3333;
 
 const routes = require("./routes/routes");
 
-// const ongsRoute = require("./routes/ongs");
-// const healthCheckRoute = require("./routes/health-check");
+const healthCheckRoute = require("./routes/health-check");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
-// app.use("/health-check", healthCheckRoute);
-// app.use("/ongs", ongsRoute);
+app.use("/health-check", healthCheckRoute);
 
 app.listen(porta, () => console.log(`Ouvindo na porta ${porta}...`));
